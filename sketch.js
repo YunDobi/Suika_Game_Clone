9674cedee5abca479b2d9b697abb9f590fde2121
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-10-30 23:13:42 -0700

    half done, and have to fix game over and merge in same time

--- sketch.js
+++ sketch.js
@@ -1,9 +1,73 @@
 let engine = Matter.Engine.create();
+engine.gravity.y = 1.5;
 let render = Matter.Render.create({
   element: document.getElementById('container'),
   engine: engine,
-  options: { wireframes: false, width: 430, height: 754 },
+  options: {
+    wireframes: false,
+    width: 430,
+    height: 754,
+    background: '#F7F4C8',
+  },
 });
+
+const Friuts = [
+  {
+    label: 'cherry',
+    radius: 40 / 2,
+    color: '#F20306',
+  },
+  {
+    label: 'strawberry',
+    radius: 50 / 2,
+    color: '#FF624C',
+  },
+  {
+    label: 'grape',
+    radius: 72 / 2,
+    color: '#A969FF',
+  },
+  {
+    label: 'kumkwat',
+    radius: 85 / 2,
+    color: '#FFAF02',
+  },
+  {
+    label: 'orange',
+    radius: 106 / 2,
+    color: '#FC8611',
+  },
+  {
+    label: 'apple',
+    radius: 140 / 2,
+    color: '#F41615',
+  },
+  {
+    label: 'pear',
+    radius: 160 / 2,
+    color: '#FDF176',
+  },
+  {
+    label: 'peach',
+    radius: 196 / 2,
+    color: '#FEB6AC',
+  },
+  {
+    label: 'pineapple',
+    radius: 220 / 2,
+    color: '#F7E608',
+  },
+  {
+    label: 'melon',
+    radius: 270 / 2,
+    color: '#89CE13',
+  },
+  {
+    label: 'watermelon',
+    radius: 300 / 2,
+    color: '#26AA1E',
+  },
+];
 const World = Matter.World;
 let current = null;
 let isClicking = false;
@@ -19,6 +83,15 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
 });
 // render.mouse = mouse;
 
+const getRandomFruit = () => {
+  const randomIndex = Math.floor(Math.random() * 5);
+  const fruit = Friuts[randomIndex];
+
+  if (current && current.label === fruit.label) return getRandomFruit();
+
+  return fruit;
+};
+
 const wall = (x, y, width, height) => {
   return Matter.Bodies.rectangle(x, y, width, height, {
     isStatic: true,
@@ -26,39 +99,43 @@ const wall = (x, y, width, height) => {
   });
 };
 
-const topLine = Matter.Bodies.rectangle(300, 0, 600, 20, {
+const topLine = Matter.Bodies.rectangle(300, 120, 600, 2, {
   isStatic: true,
   isSensor: true,
+  label: 'top',
   render: { visible: true, fillStyle: '#E6B143' },
 });
 
 World.add(engine.world, [
   mouseConstraint,
   //ground
-  wall(300, 754, 600, 20),
+  wall(300, 754, 600, 30),
   //left wall
-  wall(-10, 370, 20, 754),
+  wall(-10, 370, 50, 754),
   //right wall
-  wall(440, 370, 20, 754),
+  wall(440, 370, 50, 754),
   topLine,
 ]);
 
-// eslint-disable-next-line func-style
-function addCurrentFruit() {
-  console.log(current, falling);
-  if (falling === true) {
-    console.log('it is falling');
-    return;
-  }
-  current = Matter.Bodies.circle(300, 50, Math.floor(Math.random() * 50) + 15, {
+const addCurrentFruit = () => {
+  const randomFruit = getRandomFruit();
+
+  // if (falling === true) {
+  //   console.log('it is falling');
+  //   return;
+  // }
+  current = Matter.Bodies.circle(300, 50, randomFruit.radius, {
     // isStatic: true,
     isSleeping: true,
-    render: { fillStyle: 'white' },
+    render: { fillStyle: randomFruit.color },
+    friction: 0.05,
+    density: 0.001,
     restitution: 0.2,
+    label: randomFruit.label,
   });
-  console.log(current);
+  // console.log(current);
   World.add(engine.world, current);
-}
+};
 
 addCurrentFruit();
 
@@ -77,7 +154,7 @@ Matter.Events.on(mouseConstraint, 'mousedown', () => {
 });
 
 Matter.Events.on(mouseConstraint, 'mousemove', () => {
-  // console.log(current)
+  console.log(current)
   if (isClicking === true) {
     try {
       Matter.Body.setPosition(current, {
@@ -91,7 +168,7 @@ Matter.Events.on(mouseConstraint, 'mousemove', () => {
 });
 
 Matter.Events.on(mouseConstraint, 'mouseup', () => {
-  if (falling || current === "loading") return;
+  if (falling || current === 'loading') return;
 
   isClicking = false;
   falling = true;
@@ -105,22 +182,35 @@ Matter.Events.on(mouseConstraint, 'mouseup', () => {
 });
 
 Matter.Events.on(engine, 'collisionStart', ({ pairs }) => {
-  // console.log("dd", pairs);
-  falling = false;
   pairs.forEach(({ bodyA, bodyB }) => {
-    // console.log(bodyA.label.includes('Circle'), bodyB);
-    if (
-      bodyA.label.includes('Circle') &&
-      bodyB.label.includes('Circle') &&
-      bodyA.circleRadius === bodyB.circleRadius
-    ) {
+    // console.log("position:", bodyA.position.y, bodyB.position.y)
+    console.log(pairs[0].collision.supports[0])
+    if (bodyA.isStatic || bodyB.isStatic) return;
+    const aY = bodyA.position.y + bodyA.circleRadius;
+    const bY = bodyB.position.y + bodyB.circleRadius;
+    console.log('ay', aY, 'by', bY);
+
+    if (aY < 140 || bY < 140) {
+      alert('dd');
+      console.log('falling:', falling, 'current:', current);
+    }
+
+    if (bodyA.label === bodyB.label) {
+      const newFruit =
+        Friuts[Friuts.findIndex((fruit) => fruit.label === bodyA.label) + 1];
+
       World.remove(engine.world, [bodyA, bodyB]);
       World.add(engine.world, [
         Matter.Bodies.circle(
-          bodyA.position.x,
-          bodyA.position.y,
-          bodyA.circleRadius + 10,
-          60
+          pairs[0].collision.supports[0].x,
+          pairs[0].collision.supports[0].y,
+          newFruit.radius,
+          {
+            render: {
+              fillStyle: newFruit.color,
+            },
+            label: newFruit.label,
+          }
         ),
       ]);
     }
